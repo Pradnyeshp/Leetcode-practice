d83cac3b7b6c574b5e2fb4d39e88ea7b2ac1c283
Pradnyesh Patil <36009646+Pradnyeshp@users.noreply.github.com>
2026-06-24 16:11:06 -0700

    Time: 0 ms (100%), Space: 12.7 MB (77.63%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+
+// Are both trees guaranteed to be binary trees?
+// Can either or both trees be empty (i.e., null)?
+//  Are the node values integers?
+// I will solve this using recursion. The idea is to traverse both trees at the
+// same time and compare each corresponding pair of nodes. If at any point: tne
+// node is null and the other isn't, or both are not null but have different
+// values, then the trees are not the same.
+// If both nodes are null at the same position or if all corresponding node
+// values match and subtrees match, then the trees are identical
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        // Base cases
+        if (!p && !q)
+            return true; // both are null
+        if (!p || !q)
+            return false; // one is null, one is not
+        if (p->val != q->val)
+            return false; // values don't match
+
+        // Recursively check left and right subtrees
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
+
+// Time	O(n)	We visit each node once, where n = number of nodes
+// Space (stack)	O(h)	Due to recursion call stack, where h = height of the
+// tree
