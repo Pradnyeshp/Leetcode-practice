e3c8f6b9ade7d634a4d3db1acfc93109757a4802
Pradnyesh Patil <36009646+Pradnyeshp@users.noreply.github.com>
2026-07-12 11:39:08 -0700

    Time: 0 ms (100%), Space: 13.2 MB (79.16%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,26 +1,29 @@
 class Solution {
-  public:
-  bool searchMatrix(vector<vector<int>>& matrix, int target) {
-    int m = matrix.size();
-    if (m == 0)
-        return false;
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        if (m == 0)
+            return false;
 
-    int n = matrix[0].size();
-    // binary search
-    int left = 0, right = m * n - 1;
-    int pivotIdx, pivotElement;
-    while (left <= right) {
-      pivotIdx = (left + right) / 2;
-      pivotElement = matrix[pivotIdx / n][pivotIdx % n];
-      if (target == pivotElement)
-          return true;
-      else {
-        if (target < pivotElement)
-            right = pivotIdx - 1;
-        else
-            left = pivotIdx + 1;
-      }
+        int n = matrix[0].size();
+        // binary search
+        int left = 0, right = m * n - 1;
+        int pivotIdx, pivotElement;
+        while (left <= right) {
+            pivotIdx = (left + right) / 2;
+            int row = pivotIdx / n;
+            int col = pivotIdx % n;
+
+            pivotElement = matrix[row][col];
+            if (target == pivotElement)
+                return true;
+            else {
+                if (target < pivotElement)
+                    right = pivotIdx - 1;
+                else
+                    left = pivotIdx + 1;
+            }
+        }
+        return false;
     }
-    return false;
-  }
 };
