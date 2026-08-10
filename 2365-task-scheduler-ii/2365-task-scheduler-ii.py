2db8a720428797258f484a70e266d192789a5df5
Pradnyesh Patil <36009646+Pradnyeshp@users.noreply.github.com>
2026-08-10 11:51:23 -0700

    Time: 114 ms (20.31%), Space: 38.6 MB (65.56%) - LeetHub

---
+++ 2365-task-scheduler-ii/2365-task-scheduler-ii.py
@@ -0,0 +1,7 @@
+class Solution:
+    def taskSchedulerII(self, A, space):
+        last = defaultdict(lambda: -len(A) - 10)
+        res = 0
+        for a in A:
+            last[a] = res = max(res, last[a] + space) + 1
+        return res
